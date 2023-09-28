4999c4234758280fa1383f436406aae609761e2f
Steilor <steilormateo.44@gmail.com>
2023-09-28 12:47:05 -0400

    D & I Esteilor

---
+++ Northwind/Northwind.Infrastructure/Repositories/OrdersDetailsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Northwind.Domain.Entities;
+using Northwind.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.Infrastructure.Repositories
+{
+    public class OrdersDetailsRepository : IOrdersDetailsRepository
+    {
+        public List<OrdersDetails> GetOrdersDetailsByOrdersID(int OrdersID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
