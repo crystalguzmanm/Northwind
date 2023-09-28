dd441818a0f174bda96394ffa95393dbad542992
AdnerSantana <santana2654kk@gmail.com>
2023-09-28 18:44:11 -0400

    Atributos

--- Northwind/Northwind.Domain/Entities/Categories.cs
+++ Northwind/Northwind.Domain/Entities/Categories.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Northwind.Domain.Core;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Northwind.Domain.Entities
 {
-    internal class Categories
+    public class Categories : BaseEntity
     {
+
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public string? Description { get; set; }
+
+        public byte[] Picture { get; set; }
+
     }
 }
--- Northwind/Northwind.Domain/Entities/Employees.cs
+++ Northwind/Northwind.Domain/Entities/Employees.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using Northwind.Domain.Core;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Northwind.Domain.Entities
 {
-    internal class Employees
+    public class Employees : PersonLocation
     {
+        public int EmployeeID { get; set; }
+
+        public int LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? TitleOfCourtesy { get; set; }
+
+        public DateTime? BirthDate { get; set;}
+
+        public DateTime? HireDate { get; set; }
+
+        public string? HomePhone { get; set; }
+
+        public string? Extencion { get; set; }
+
+        public byte? Photo { get; set; }
+
+        public string? Notes { get; set; }
+
     }
 }
