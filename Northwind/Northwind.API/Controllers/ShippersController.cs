48a73753088adeab26633fcde8d28319918297df
costa <costa@DESKTOP-EOOHF5T>
2023-10-01 14:24:41 -0400

    Subida Conexion base de datos y api web Gregory

---
+++ Northwind/Northwind.API/Controllers/ShippersController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Domain.Entities;
+using Northwind.Domain.Repository;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Northwind.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShippersController : ControllerBase
+    {
+        private readonly IShippersRepository shippersRepository;
+
+        public ShippersController(IShippersRepository shippersRepository)
+        { 
+          this.shippersRepository = shippersRepository;
+        }
+
+
+        // GET: api/<ShippersController>
+        [HttpGet]
+        public IEnumerable<Shippers> Get()
+        {
+           var Shippers = this.shippersRepository.GetShippers();
+            return Shippers;
+
+
+        }
+
+        // GET api/<ShippersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ShippersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ShippersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ShippersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Northwind/Northwind.API/Program.cs
+++ Northwind/Northwind.API/Program.cs
@@ -1,6 +1,19 @@
+using Northwind.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Domain.Repository;
+using Northwind.Infrastructure.Repositories;
+using Northwind.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//Agregar dependencias del context //
+builder.Services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("NorthwindContext")));
+
+//Dependecia de los repositorio//
+builder.Services.AddTransient<IShippersRepository,ShippersRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
