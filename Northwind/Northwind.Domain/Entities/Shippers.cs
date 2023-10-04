0031174aad897f3d67562006403f3f0f1ab20670
costa <costa@DESKTOP-EOOHF5T>
2023-10-04 18:35:52 -0400

    OK

--- Northwind/Northwind.Domain/Entities/Shippers.cs
+++ Northwind/Northwind.Domain/Entities/Shippers.cs
@@ -1,11 +1,13 @@
 ﻿
 using Northwind.Domain.Core;
+using System.ComponentModel.DataAnnotations;
 
 namespace Northwind.Domain.Entities
 {
     public class Shippers : Person
     {
+        [Key]
         public int ShipperID {  get; set; } 
-        public string? HomePage { get; set; }  
+        
     }
 }
